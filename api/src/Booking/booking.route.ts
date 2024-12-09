ea2d3cfe46a244b8cb06c617f5e0db647a41b42c
Suppakorn <suwannapop01@gmail.com>
2024-12-09 18:42:53 +0700

    Update Docker configuration, modify Prisma schema, and adjust environment variables for database connection

--- api/src/Booking/booking.route.ts
+++ api/src/Booking/booking.route.ts
@@ -2,8 +2,8 @@ import express,{ Router } from "express";
 import { bookingController } from "./booking.controller";
 const router: Router = express.Router();
 
-router.get("/AllBooking", bookingController.getAllBooking);
-router.get("/NameBooking/:firstname/:lastname", bookingController.getBooking);
+router.get("/AllBooking", bookingController.getAllBooking); //doctor
+router.get("/NameBooking/:firstname/:lastname", bookingController.getBooking); //patient
 router.post("/", bookingController.createBooking);
 router.patch("/", bookingController.updateBooking);
 router.delete("/", bookingController.deleteBooking);
