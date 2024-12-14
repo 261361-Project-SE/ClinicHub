c732fef1b930f45f69b625c4df4a4e9055e12d43
4z4le4 <Kong_led_@hotmail.com>
2024-12-14 20:11:41 +0700

    Refactor calendar page iframe styles for responsive design

--- client/app/calendar/page.tsx
+++ client/app/calendar/page.tsx
@@ -5,11 +5,7 @@ const CalendarPage: React.FC = () => {
     <div className="flex justify-center items-center h-screen">
       <iframe
         src="https://calendar.google.com/calendar/embed?src=mongkolclinic.calendar%40gmail.com&ctz=Asia%2FBangkok"
-        className="border-0"
-        width="800"
-        height="600"
-        frameBorder="0"
-        scrolling="no"
+        className="border-0 w-full h-full"
       ></iframe>
     </div>
   );
