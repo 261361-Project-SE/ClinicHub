a0e12d7f663bef1a436252c8891c75016569c2d7
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-12-16 22:22:49 +0700

    fix: re structure files

--- client/app/(pages)/p/appointment/layout.tsx
+++ client/app/(pages)/p/appointment/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "./booking/components/navbar";
+import "../../../globals.css";
+import Navbar from "../appointment/components/navbar";
 import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
