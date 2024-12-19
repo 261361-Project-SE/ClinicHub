853981aae136fb94c6fc4020de16f58749e64b68
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-12-19 22:07:23 +0700

    feat: import BookingPage in Home component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
-import BookingPage from "./(pages)/p/appointment/page";
 import RootLayout from "./(pages)/p/appointment/layout";
+import BookingPage from "./(pages)/p/appointment/page";
+
 export default function Home() {
   return (
     <RootLayout>
