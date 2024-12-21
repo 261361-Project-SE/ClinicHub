dd54657121c4855554a2ae7fc4ecce1b8ea1ce1c
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-12-21 18:37:39 +0700

    feat: create CalendarPage component with embedded Google Calendar

---
+++ client/app/(pages)/d/calendar/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CanlendarPage = () => {
+  return (
+    <div className="bh-white">
+      <iframe
+        src="https://calendar.google.com/calendar/u/0/embed?src=mongkolclinic.calendar@gmail.com&ctz=Asia/Bangkok"
+        width="100%"
+        height="600"
+        className="rounded-xl bg-white shadow-shadow-bg"
+      ></iframe>
+    </div>
+  );
+};
+
+export default CanlendarPage;
