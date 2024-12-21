96936bf9f162d5b91069b55eb90373049b504ba6
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-12-21 18:36:54 +0700

    feat: add TypeScript types for Appointment and Patient tables

---
+++ client/lib/types.ts
@@ -0,0 +1,37 @@
+export type AppointmentTableProps = {
+  appointments: {
+    id: string;
+    eventId: string;
+    firstname: string;
+    lastname: string;
+    phone_number: string;
+    symptom: string;
+    appointment_dateTime: string;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+  }[];
+};
+
+export type PatientTableProps = {
+  patients: {
+    PatientID: number; // Primary key
+    FirstName: string;
+    LastName: string;
+    Age: number;
+    Gender: "Male" | "Female" | "Other"; // ENUM
+    CitizenID: string; // National ID
+    BirthDate: string; // Date of birth
+    Address: string; // Address
+    Phone: string; // Phone number
+    EmergencyContact: string; // Emergency contact
+    Status: string; // Patient status
+    Occupation: string; // Job
+    Height: number; // Height in cm
+    Weight: number; // Weight in kg
+    ChronicDisease: string; // Chronic diseases
+    Allergies: string; // Allergies
+    created_at: string; // Timestamp
+    updated_at: string; // Timestamp
+  }[];
+};
