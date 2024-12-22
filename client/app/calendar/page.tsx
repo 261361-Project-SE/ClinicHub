530aba764eab77d0d5fe53a7bd5d20d13cae3754
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-12-22 07:44:55 +0700

    refactor: adjust layout and iframe styling in CalendarPage component

--- client/app/calendar/page.tsx
+++ client/app/calendar/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const CalendarPage: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex items-center justify-center">
       <iframe
         src="https://calendar.google.com/calendar/embed?src=mongkolclinic.calendar%40gmail.com&ctz=Asia%2FBangkok"
-        className="border-0 w-full h-full"
+        className="w-full border-0"
       ></iframe>
     </div>
   );
