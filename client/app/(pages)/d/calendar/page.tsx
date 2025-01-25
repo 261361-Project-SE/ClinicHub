7c36b1960a88bbcb0f03db49a44513b1354a74b8
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-25 18:28:25 +0700

    feat: add title attribute to iframe in CalendarPage for improved accessibility

--- client/app/(pages)/d/calendar/page.tsx
+++ client/app/(pages)/d/calendar/page.tsx
@@ -4,6 +4,7 @@ const CanlendarPage = () => {
   return (
     <div className="flex items-center justify-center h-full bg-white rounded-xl shadow-shadow-bg">
       <iframe
+        title="appointment-calendar"
         src="https://calendar.google.com/calendar/embed?src=mongkolclinic.calendar%40gmail.com&ctz=Asia%2FBangkok"
         className="w-full h-full border-0 rounded-xl"
       ></iframe>
