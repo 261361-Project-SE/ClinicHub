39b42eebf8edd6790814247c2bdfec2466d74f57
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-25 18:29:41 +0700

    feat: enhance AppointmentCard component with detailed appointment information and improved layout

--- client/components/dashboard/AppointmentCard.tsx
+++ client/components/dashboard/AppointmentCard.tsx
@@ -1,23 +1,70 @@
-import { CalendarClockIcon } from "lucide-react";
-import React from "react";
+import { ClipboardPlusIcon, Clock4Icon } from "lucide-react";
 
-const AppointmentCard = ({
-  title,
-  count,
-  date,
-}: {
-  title: string;
-  count: string;
-  date: string;
-}) => (
-  <div className="flex items-center justify-center w-1/3 p-4 bg-white rounded-xl shadow-shadow-bg gap-8">
-    <CalendarClockIcon color="#FB6F92" size={84} />
-    <div className="flex flex-col gap-y-2">
-      <div className="text-3xl text-darkgray">{title}</div>
-      <div className="text-4xl">{count}</div>
-      <div className="text-lightgray">{date}</div>
+import { formattedDateTime } from "@/app/utils/formattedDateTime";
+import { AppointmentPopover } from "@/components/dashboard/AppointmentPopover";
+import { AppointmentProps } from "@/lib/types";
+import { AppointmentStatus } from "@/lib/variables";
+
+const AppointmentCard: React.FC<AppointmentProps> = (props) => {
+  const {
+    id,
+    firstname,
+    lastname,
+    symptom,
+    appointment_dateTime,
+    selectedFilter,
+    appointment_status,
+  } = props as AppointmentProps & {
+    appointment_status: keyof typeof AppointmentStatus;
+  };
+  const { shortDate, time, weekday } = formattedDateTime(appointment_dateTime);
+  const currentDate = new Date();
+  const isSameDate =
+    new Date(appointment_dateTime).getDate() === currentDate.getDate();
+  const endOfBookingTime =
+    new Date(appointment_dateTime).getTime() + 15 * 60000;
+  const formattedEndOfBookingTime = formattedDateTime(endOfBookingTime).time;
+  const isHistoryOrCanceled =
+    selectedFilter === "canceled" || selectedFilter === "history";
+
+  const statusInfo = AppointmentStatus[appointment_status] || {
+    label: "สถานะไม่ทราบ",
+  };
+
+  return (
+    <div
+      key={id}
+      className="flex items-center py-1 border border-[##E5E5E5] gap-x-4 rounded-xl"
+    >
+      <div
+        className={`flex flex-col items-center w-1/12 gap-y-2 border-r p-1 border-r-[#E5E5E5] font-medium ${
+          isSameDate ? "text-pink-200" : "text-darkgray"
+        }`}
+      >
+        <div>{weekday}</div>
+        <div className="text-2xl">{shortDate}</div>
+      </div>
+      <div className="flex items-start justify-between w-11/12 mr-4 gap-x-16">
+        <div className="flex gap-x-32">
+          <div className="flex flex-col gap-y-1">
+            <div className="flex items-center text-sm gap-x-1 text-darkgray">
+              <Clock4Icon className="mt-0.5" size={14} />
+              {time} - {formattedEndOfBookingTime}
+            </div>
+            <div className="flex items-center text-sm gap-x-1 text-darkgray">
+              <ClipboardPlusIcon className="mt-0.5" size={14} /> {symptom}
+            </div>
+          </div>
+          <div>
+            <div>
+              คนไข้ {firstname} {lastname}
+            </div>
+          </div>
+        </div>
+        <div>{!isHistoryOrCanceled && <AppointmentPopover {...props} />}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AppointmentCard;
