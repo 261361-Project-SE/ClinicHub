954a165c36dcad81c9481b6d6bdeba989043272b
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-26 23:18:27 +0700

    feat: add appointment time formatting and empty state handling in AppointmentTable component

--- client/components/dashboard/AppointmentTable.tsx
+++ client/components/dashboard/AppointmentTable.tsx
@@ -1,3 +1,4 @@
+import { formattedDateTime } from "@/app/utils/formattedDateTime";
 import {
   Table,
   TableBody,
@@ -11,29 +12,42 @@ import { AppointmentTableProps } from "@/lib/types";
 const AppointmentTable: React.FC<AppointmentTableProps> = ({
   appointments,
 }) => {
+  const getTime = (appointments_dateTime: string) => {
+    const { time }: { time: string } = formattedDateTime(appointments_dateTime);
+    return time;
+  };
+
   return (
     <Table>
       <TableHeader>
         <TableRow className="text-darkgray">
-          <TableHead className="w-[100px]">หมายเลขนัด</TableHead>
+          <TableHead className="w-[10px]">ไอดี</TableHead>
           <TableHead>คนไข้</TableHead>
           <TableHead>อาการ</TableHead>
           <TableHead className="text-right">เวลา</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {appointments.slice(0, 4).map((appointment) => (
-          <TableRow key={appointment.id}>
-            <TableCell className="font-medium">{appointment.id}</TableCell>
-            <TableCell>
-              {appointment.firstname} {appointment.lastname}
-            </TableCell>
-            <TableCell>{appointment.symptom}</TableCell>
-            <TableCell className="text-right">
-              {appointment.appointment_status}
+        {appointments.length === 0 ? (
+          <TableRow>
+            <TableCell colSpan={4} className="text-center text-darkgray">
+              ไม่มีการนัดหมาย
             </TableCell>
           </TableRow>
-        ))}
+        ) : (
+          appointments.slice(0, 4).map((appointment) => (
+            <TableRow key={appointment.id}>
+              <TableCell className="font-medium">{appointment.id}</TableCell>
+              <TableCell>
+                {appointment.firstname} {appointment.lastname}
+              </TableCell>
+              <TableCell>{appointment.symptom}</TableCell>
+              <TableCell className="text-right">
+                {getTime(appointment.appointment_dateTime)}
+              </TableCell>
+            </TableRow>
+          ))
+        )}
       </TableBody>
     </Table>
   );
