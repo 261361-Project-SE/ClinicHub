f0a8687d30b76a8ea0c4191f0beda98b836be5ee
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-26 23:22:46 +0700

    feat: implement appointment creation with async handling and loading state

--- client/components/dashboard/CreateAppointmentDialog.tsx
+++ client/components/dashboard/CreateAppointmentDialog.tsx
@@ -1,9 +1,12 @@
-import { PlusIcon } from "lucide-react";
+import axios from "axios";
+import { Loader2, PlusIcon } from "lucide-react";
 import { useState } from "react";
+import { toast } from "sonner";
 
 import { AppointmentDatePicker } from "@/components/dashboard/AppointmentDatePicker";
 import { AppointmentTimeSelector } from "@/components/dashboard/AppointmentTimeSelector";
 import { ConfirmButton } from "@/components/dashboard/ConfirmButton";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -14,70 +17,86 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { AppointmentProps } from "@/lib/types";
+import { SERVER_URL } from "@/lib/variables";
 
 const CreateAppointmentDialog = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [formState, setFormState] = useState<AppointmentProps>({
+  const [isAppointmentModalOpen, setIsAppointmentModalOpen] = useState(false);
+  const [isCreatingAppointment, setIsAppointmentCreating] = useState(false);
+  const [appointmentForm, setAppointmentForm] = useState({
     firstname: "",
     lastname: "",
     phone_number: "",
     symptom: "",
     appointment_date: "",
     appointment_time: "",
-    appointment_status: "",
   });
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { id, value } = e.target;
-    setFormState((prevState) => ({
+    setAppointmentForm((prevState) => ({
       ...prevState,
       [id]: value,
     }));
   };
 
-  const handleConfirm = () => {
-    const { firstname, lastname, symptom, appointment_date, appointment_time } =
-      formState;
+  const handleConfirmCreateAppointment = async () => {
+    const {
+      firstname,
+      lastname,
+      phone_number,
+      symptom,
+      appointment_date,
+      appointment_time,
+    } = appointmentForm;
 
     if (
       !firstname ||
       !lastname ||
+      !phone_number ||
       !appointment_date ||
       !appointment_time ||
       !symptom
     ) {
-      alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+      toast.error("กรุณากรอกข้อมูลให้ครบถ้วน");
       return;
     }
+
     const formattedTime = `${appointment_time.slice(
       0,
       2
     )}:${appointment_time.slice(2)}`;
-    const dateTimeString = `${appointment_date}T${formattedTime}:00`;
-    const timestamp = new Date(dateTimeString).toISOString();
+    const dateTimeString = `${appointment_date}T${formattedTime}:00.000`;
 
-    console.log("Form submitted with timestamp:", timestamp);
-    console.log("Form data:", formState);
+    setIsAppointmentCreating(true);
+    try {
+      await axios.post(`${SERVER_URL}/appointment/create`, {
+        ...appointmentForm,
+        appointment_dateTime: dateTimeString,
+      });
+      toast.success("การนัดหมายถูกสร้างเรียบร้อยแล้ว!");
 
-    // API call or further logic for appointment creation
-
-    setFormState({
-      firstname: "",
-      lastname: "",
-      phone_number: "",
-      symptom: "",
-      appointment_date: "",
-      appointment_time: "",
-      appointment_status: "",
-    });
-    setIsOpen(false);
+      setAppointmentForm({
+        firstname: "",
+        lastname: "",
+        phone_number: "",
+        symptom: "",
+        appointment_date: "",
+        appointment_time: "",
+      });
+      setIsAppointmentCreating(false);
+      setIsAppointmentModalOpen(false);
+    } catch (error) {
+      toast.error("มีข้อผิดพลาดเกิดขึ้น กรุณาลองใหม่อีกครั้ง");
+    }
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog
+      open={isAppointmentModalOpen}
+      onOpenChange={setIsAppointmentModalOpen}
+    >
       <DialogTrigger className="flex items-center px-4 font-medium text-white bg-pink-200 hover:bg-pink-200/90 rounded-2xl h-14">
         <PlusIcon className="mr-1" />
         สร้างการนัดหมาย
@@ -92,44 +111,55 @@ const CreateAppointmentDialog = () => {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            handleConfirm();
+            handleConfirmCreateAppointment();
           }}
         >
-          <div className="flex flex-col mt-4 gap-y-4 text-darkgray">
+          <div className="flex flex-col gap-y-4 text-darkgray">
             <div className="flex flex-col gap-y-2">
-              <Label htmlFor="firstname">ชื่อคนไข้</Label>
               <div className="flex gap-2">
-                <Input
-                  type="text"
-                  id="firstname"
-                  placeholder="ชื่อ"
-                  value={formState.firstname}
-                  onChange={handleInputChange}
-                />
-                <Input
-                  type="text"
-                  id="lastname"
-                  placeholder="นามสกุล"
-                  value={formState.lastname}
-                  onChange={handleInputChange}
-                />
+                <div className="w-1/2">
+                  <Label htmlFor="firstname">ชื่อ</Label>
+                  <Input
+                    type="text"
+                    id="firstname"
+                    value={appointmentForm.firstname}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Label htmlFor="lastname">นามสกุล</Label>
+                  <Input
+                    type="text"
+                    id="lastname"
+                    value={appointmentForm.lastname}
+                    onChange={handleInputChange}
+                  />
+                </div>
               </div>
             </div>
+            <Label htmlFor="phone_number">เบอร์โทรศัพท์</Label>
+            <Input
+              type="text"
+              id="phone_number"
+              maxLength={10}
+              value={appointmentForm.phone_number}
+              onChange={handleInputChange}
+            />
             <div className="flex flex-col gap-y-2">
               <Label htmlFor="symptom">บันทึกย่อ</Label>
               <Textarea
                 placeholder="คนไข้ไม่สบายตัว ปวดหัว"
                 id="symptom"
-                value={formState.symptom}
+                value={appointmentForm.symptom}
                 onChange={handleInputChange}
               />
             </div>
             <div className="flex flex-col gap-y-2">
               <Label htmlFor="appointment_date">วันที่</Label>
               <AppointmentDatePicker
-                value={formState.appointment_date}
+                value={appointmentForm.appointment_date}
                 setValue={(value) =>
-                  setFormState((prevState) => ({
+                  setAppointmentForm((prevState) => ({
                     ...prevState,
                     appointment_date: value,
                   }))
@@ -139,23 +169,30 @@ const CreateAppointmentDialog = () => {
             <div className="flex flex-col gap-y-2">
               <Label htmlFor="appointment_time">เวลานัด</Label>
               <AppointmentTimeSelector
-                appointment_time={formState.appointment_time}
+                appointment_time={appointmentForm.appointment_time}
                 setValue={(value) =>
-                  setFormState((prevState) => ({
+                  setAppointmentForm((prevState) => ({
                     ...prevState,
                     appointment_time: value,
                   }))
                 }
               />
             </div>
-            <ConfirmButton
-              buttonTitle="สร้างการนัดหมาย"
-              title="สร้างการนัดหมาย"
-              description="คุณแน่ใจหรือไม่ว่าต้องการสร้างการนัดหมายนี้"
-              cancelTitle="ยกเลิก"
-              confirmTitle="สร้างการนัดหมาย"
-              handleConfirm={handleConfirm}
-            />
+            {isCreatingAppointment ? (
+              <Button disabled>
+                <Loader2 className="animate-spin" />
+                กำลังสร้างการนัดหมาย...
+              </Button>
+            ) : (
+              <ConfirmButton
+                buttonTitle="สร้างการนัดหมาย"
+                title="ยืนยันการสร้างการนัดหมาย"
+                description="คุณแน่ใจหรือไม่ว่าต้องการสร้างการนัดหมายนี้?"
+                cancelTitle="ยกเลิก"
+                confirmTitle="ยืนยัน"
+                onConfirm={handleConfirmCreateAppointment}
+              />
+            )}
           </div>
         </form>
       </DialogContent>
