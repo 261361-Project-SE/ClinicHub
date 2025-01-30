0295a613b3bb3220f5aeae2809b6a5aee6bb1a83
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-30 15:42:44 +0700

    feat: add mounted state to App component for conditional rendering

--- client/app/_app.tsx
+++ client/app/_app.tsx
@@ -2,17 +2,19 @@
 
 import { useAppointmentStore } from "@/stores/useAppointmentStore";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isMounted, setIsMounted] = useState(false);
   const fetchAppointments = useAppointmentStore(
     (state) => state.fetchAppointments
   );
 
   useEffect(() => {
     fetchAppointments();
+    setIsMounted(true);
   }, [fetchAppointments]);
 
-  return <Component {...pageProps} />;
+  return isMounted ?? <Component {...pageProps} />;
 }
