5fa48cabedad70fdfaf1048ed3d5e43de71850eb
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-31 23:58:53 +0700

    fix: hydration errors

--- client/app/(pages)/d/layout.tsx
+++ client/app/(pages)/d/layout.tsx
@@ -3,6 +3,7 @@
 import Searchbar from "@/components/dashboard/Searchbar";
 import Sidebar from "@/components/ui/DashboardSidebar";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const PAGE_TITLES: Record<string, string> = {
   "/d/dashboard": "แดชบอร์ด",
@@ -17,31 +18,32 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const pathname = usePathname();
-  const pageTitle = PAGE_TITLES[pathname] || "";
+  const [pageTitle, setPageTitle] = useState("");
+
+  useEffect(() => {
+    setPageTitle(PAGE_TITLES[pathname] || "");
+  }, [pathname]);
+
   return (
-    <html lang="en">
-      <body>
-        <main className="flex h-screen bg-whitegray-100">
-          <aside className="w-64 h-full xl:w-72 2xl:w-80">
-            <Sidebar />
-          </aside>
+    <main className="flex h-screen bg-whitegray-100">
+      <aside className="w-64 h-full xl:w-72 2xl:w-80">
+        <Sidebar />
+      </aside>
 
-          <section className="flex-1 h-full p-5">
-            <header className="flex items-center justify-between h-[86px] px-6 bg-white shadow-shadow-bg rounded-2xl">
-              <h1 className="text-2xl font-medium text-darkgray md:text-3xl xl:text-4xl">
-                {pageTitle}
-              </h1>
-              <div className="min-w-[300px]">
-                <Searchbar />
-              </div>
-            </header>
+      <section className="flex-1 h-full p-5">
+        <header className="flex items-center justify-between h-[86px] px-6 bg-white shadow-shadow-bg rounded-2xl">
+          <h1 className="text-2xl font-medium text-darkgray md:text-3xl xl:text-4xl">
+            {pageTitle}
+          </h1>
+          <div className="min-w-[300px]">
+            <Searchbar />
+          </div>
+        </header>
 
-            <div className="h-[calc(100vh-130px)] mt-5 overflow-auto">
-              {children}
-            </div>
-          </section>
-        </main>
-      </body>
-    </html>
+        <div className="h-[calc(100vh-130px)] mt-5 overflow-auto">
+          {children}
+        </div>
+      </section>
+    </main>
   );
 }
