24b6eee28b2a931632520a7193e358eb1b7d9818
Ton Natthapon <nattapon_ton_np@hotmail.com>
2025-01-31 22:41:16 +0700

    refactor: replace Calendar component with native date input for improved usability

--- client/components/dashboard/AppointmentDatePicker.tsx
+++ client/components/dashboard/AppointmentDatePicker.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import { format } from "date-fns";
-import { th } from "date-fns/locale";
-import { Calendar as CalendarIcon } from "lucide-react";
-import { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
@@ -13,6 +8,10 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { th } from "date-fns/locale";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { useState } from "react";
 
 export function AppointmentDatePicker({
   setValue,
@@ -48,11 +47,18 @@ export function AppointmentDatePicker({
         </Button>
       </PopoverTrigger>
       <PopoverContent>
-        <Calendar
+        {/* <Calendar
           locale={th}
           mode="single"
+          className="z-10"
           selected={date || undefined}
           onSelect={(day) => handleDateChange(day)}
+        /> */}
+        <input
+          type="date"
+          className="w-full p-2 border border-gray-300 rounded-md"
+          value={format(date || new Date(), "yyyy-MM-dd")}
+          onChange={(e) => handleDateChange(new Date(e.target.value))}
         />
       </PopoverContent>
     </Popover>
