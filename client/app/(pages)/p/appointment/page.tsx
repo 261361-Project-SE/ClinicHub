0b4c466ae6a2448cbefca0a3c796f6001c330c94
tteenntt <87744652+Unlxii@users.noreply.github.com>
2025-03-06 17:09:33 +0700

    improve font and layout

--- client/app/(pages)/p/appointment/page.tsx
+++ client/app/(pages)/p/appointment/page.tsx
@@ -56,7 +56,7 @@ const BookingPage: React.FC = () => {
                 className="icon-size w-20 h-20 sm:w-24 sm:h-24"
                 strokeWidth={2}
               />
-              <span className="text-3xl sm:text-3xl font-noto">จองการนัด</span>
+              <span className=" text-4xl sm:text-3xl font-noto">จองการนัด</span>
             </button>
           </Link>
 
@@ -83,7 +83,7 @@ const BookingPage: React.FC = () => {
 
       {/* iPad and Desktop Views */}
       <div className="hidden md:block container mx-auto justify-center items-center px-4 py-2">
-        <div className="bg-white rounded-2xl p-6 lg:p-8 mb-5 shadow-md h-[300px] sm:h-[350px] md:h-[350px] lg:h-[400px] w-full">
+        <div className="bg-white  mb-5 shadow-md  sm:h-[350px] md:h-[350px] lg:h-[400px] w-full h-full">
           <ImageSlider images={images} />
         </div>
         <div className="flex items-center justify-center gap-4 sm:gap-6 md:gap-6">
@@ -95,9 +95,7 @@ const BookingPage: React.FC = () => {
                 className="icon-size w-20 h-20 sm:w-24 sm:h-24 md:w-24 md:h-24"
                 strokeWidth={2}
               />
-              <span className="text-3xl sm:text-3xl md:text-4xl font-noto">
-                จองการนัด
-              </span>
+              <span className="mt-4 text-4xl font-medium">จองการนัด</span>
             </button>
           </Link>
 
@@ -116,7 +114,9 @@ const BookingPage: React.FC = () => {
               className={`${buttonClasses} flex flex-col items-center justify-center w-full sm:w-[300px] md:w-[350px] lg:w-[400px] h-[200px] sm:h-[225px] md:h-[225px] lg:h-[255px] bg-green-300 text-white rounded-2xl transition-transform duration-200`}
             >
               <MessagesSquare className="icon-size w-24 h-24" strokeWidth={2} />
-              <span className="text-xl font-noto pt-4">ประเมินความพึงพอใจ</span>
+              <span className="mt-4 text-4xl font-medium">
+                ประเมินความพึงพอใจ
+              </span>
             </button>
           </Link>
         </div>
